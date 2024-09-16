11d7fa16f37678324069e705be642511bcc5dde0
omocha14 <omocha14@yahoo.co.jp>
2024-09-16 10:11:48 +0900

    README

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AttendanceController;
+use App\Http\Controllers\AuthenticatedSessionController;
+use App\Http\Controllers\SampleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,7 +15,31 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+//ログインホーム
+Route::get('/',[AttendanceController::class,'punch']);
+//    ->middleware('auth','verified');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 打刻機能
+Route::post('/work', [AttendanceController::class, 'work'])
+    ->name('work');
+
+// ログアウト
+Route::get('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
+
+// 管理ページ / 日付別
+Route::get('/attendance/date', [AttendanceController::class, 'indexDate'])
+    ->name('attendance/date');
+Route::post('/attendance/date', [AttendanceController::class, 'perDate'])
+    ->name('per/date');
+
+// 管理ページ / ユーザー別
+Route::get('/attendance/user', [AttendanceController::class, 'indexUser'])
+    ->name('attendance/user');
+Route::post('/attendance/user', [AttendanceController::class, 'perUser'])
+    ->name('per/user');
+
+// 管理ページ / ユーザー一覧
+Route::get('/user', [AttendanceController::class, 'user'])
+    ->name('user');
